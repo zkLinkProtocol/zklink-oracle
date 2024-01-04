56cc61fae35102880667b92d3b527e77f210a3ce
jimzk <jimzkx@gmail.com>
2024-01-04 15:44:26 +0800

    Add native poseidon hash

--- src/gadgets/poseidon.rs
+++ src/gadgets/poseidon.rs
@@ -4,15 +4,20 @@ use sync_vm::{
         bellman::{plonk::better_better_cs::cs::ConstraintSystem, SynthesisError},
         plonk::circuit::allocated_num::Num,
     },
-    rescue_poseidon::{CircuitGenericSponge, PoseidonParams},
+    rescue_poseidon::{CircuitGenericSponge, GenericSponge, PoseidonParams},
 };
+const WIDTH: usize = 3;
+const RATE: usize = 2;
 
 pub fn circuit_poseidon_hash<E: Engine, CS: ConstraintSystem<E>>(
     cs: &mut CS,
     input: &[Num<E>],
-) -> Result<[Num<E>; 2], SynthesisError> {
-    const WIDTH: usize = 3;
-    const RATE: usize = 2;
+) -> Result<[Num<E>; RATE], SynthesisError> {
     let params = PoseidonParams::<E, RATE, WIDTH>::default();
     CircuitGenericSponge::hash_num(cs, input, &params, None)
+}
+
+pub fn poseidon_hash<E: Engine>(input: &[E::Fr]) -> [E::Fr; RATE] {
+    let params = PoseidonParams::<E, RATE, WIDTH>::default();
+    GenericSponge::hash(input, &params, None)
 }

e042bf6eaebc39432272175c17c388025300b32a
jimzk <jimzkx@gmail.com>
2024-01-17 17:50:55 +0800

    Add check_by_address for redstone circuit

--- src/redstone/circuit.rs
+++ src/redstone/circuit.rs
@@ -1,9 +1,15 @@
 use pairing::Engine;
 use sync_vm::{
-    circuit_structures::byte::Byte,
-    franklin_crypto::bellman::{plonk::better_better_cs::cs::ConstraintSystem, SynthesisError},
+    circuit_structures::byte::{Byte, IntoBytes},
+    franklin_crypto::{
+        bellman::{plonk::better_better_cs::cs::ConstraintSystem, SynthesisError},
+        plonk::circuit::boolean::Boolean,
+    },
     traits::CSAllocatable,
+    vm::primitives::uint256::UInt256,
 };
+
+use crate::gadgets::{ecdsa::Signature, ethereum::Address};
 
 use super::types::{DataPackage, DataPoint};
 
@@ -40,6 +46,69 @@ impl<E: Engine> CircuitDataPoint<E> {
     }
 }
 
+pub struct CircuitSignedDataPackage<E: Engine> {
+    pub data_package: CircuitDataPackage<E>,
+    pub signature: Signature<E>,
+}
+
+impl<E: Engine> CircuitSignedDataPackage<E> {
+    pub fn from_witness<CS: ConstraintSystem<E>>(
+        cs: &mut CS,
+        data_package: DataPackage,
+        signature: [u8; 65],
+    ) -> Result<Self, SynthesisError> {
+        let data_package = CircuitDataPackage::from_witness(cs, data_package)?;
+        let signature = Signature::from_bytes_witness(cs, &signature)?;
+        Ok(Self {
+            data_package,
+            signature,
+        })
+    }
+
+    pub fn ecrecover<CS: ConstraintSystem<E>>(
+        &self,
+        cs: &mut CS,
+    ) -> Result<crate::gadgets::ecdsa::EcRecoverRes<E>, SynthesisError> {
+        let msg_hash = {
+            let bytes = self.data_package.serialize()?;
+            use crate::gadgets::keccak256::digest;
+            let hash = digest(cs, &bytes)?;
+            UInt256::from_be_bytes_fixed(cs, &hash)?
+        };
+
+        let pubkey = self.signature.ecrecover(cs, &msg_hash)?;
+        Ok(pubkey)
+    }
+
+    pub fn check_by_address<CS: ConstraintSystem<E>>(
+        &self,
+        cs: &mut CS,
+        guardian_set: &[Address<E>],
+    ) -> Result<Boolean, SynthesisError> {
+        if guardian_set.is_empty() {
+            return Ok(Boolean::Constant(false));
+        }
+        let (successful, (x, y)) = self.ecrecover(cs)?;
+
+        let is_matched = {
+            let (x, y) = (
+                x.into_be_bytes(cs)?.try_into().unwrap(),
+                y.into_be_bytes(cs)?.try_into().unwrap(),
+            );
+            let address = Address::from_pubkey(cs, &x, &y)?;
+            let mut is_matched = Boolean::constant(false);
+            for (_, guardian) in guardian_set.iter().enumerate() {
+                let is_equal = guardian.equals(cs, &address)?;
+                is_matched = Boolean::or(cs, &is_matched, &is_equal)?;
+            }
+            is_matched
+        };
+
+        let is_ok = Boolean::and(cs, &is_matched, &successful)?;
+        Ok(is_ok)
+    }
+}
+
 pub struct CircuitDataPackage<E: Engine> {
     pub data_points: Vec<CircuitDataPoint<E>>,
     pub timestamp: [Byte<E>; super::TIMESTAMP_BS],
@@ -48,7 +117,7 @@ pub struct CircuitDataPackage<E: Engine> {
 }
 
 impl<E: Engine> CircuitDataPackage<E> {
-    pub fn from_data_point<CS: ConstraintSystem<E>>(
+    pub fn from_witness<CS: ConstraintSystem<E>>(
         cs: &mut CS,
         witness: DataPackage,
     ) -> Result<Self, SynthesisError> {
@@ -123,7 +192,7 @@ mod tests {
             1654353400000u64,
         );
         let cs = &mut create_test_constraint_system()?;
-        let circuit_data_package = CircuitDataPackage::from_data_point(cs, data_package)
+        let circuit_data_package = CircuitDataPackage::from_witness(cs, data_package)
             .expect("should create circuit data package");
         let hash = circuit_data_package.keccak256_hash(cs)?;
         bytes_assert_eq(
@@ -131,6 +200,11 @@ mod tests {
             "e27cdb508629d3bbbb93739f48f282e89374eb5ea105cf519abd68a249cc2070",
         );
         println!("DONE");
+        Ok(())
+    }
+
+    #[test]
+    fn test_check_by_address() -> Result<(), SynthesisError> {
         Ok(())
     }
 }

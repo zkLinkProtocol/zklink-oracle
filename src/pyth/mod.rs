f7666aad45755570eb2b6eb0836b9991ef1304f1
jimzk <jimzkx@gmail.com>
2024-01-25 16:52:09 +0800

    Add default guardian set for pyth

--- src/pyth/mod.rs
+++ src/pyth/mod.rs
@@ -1,8 +1,10 @@
 pub mod circuit;
+mod params;
 mod price;
 mod wormhole;
 
 pub const WIDTH_PRICE_FEED_BYTES: usize = price::LEN_PRICE_FEED;
 pub use circuit::*;
+pub use params::*;
 pub use price::*;
 pub use wormhole::*;
